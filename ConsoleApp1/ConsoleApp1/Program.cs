98739c9b2d8cdfcb289ede9026698d03633c2770
MykytaLysenko <56317428+MykytaLysenko@users.noreply.github.com>
2022-10-17 20:55:25 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prosze wybrać :");
+            Console.WriteLine("1. Zalożyc biżuterie");
+            Console.WriteLine("2. Wykupic biżuterie");
+            ConsoleKey key = GetButton();
+            Console.Clear();
+
+            if (key == ConsoleKey.D1)
+            {
+                Console.WriteLine("Obrales 1");
+            }
+            else if (key == ConsoleKey.D2)
+            {
+                Console.WriteLine("Obrales 2");
+            }
+
+            Console.ReadKey();
+            
+        }
+
+        public static ConsoleKey GetButton()
+        {
+            var but = Console.ReadKey().Key;
+
+            return but;
+        }
+
+        
+    }
+
+    
+}
